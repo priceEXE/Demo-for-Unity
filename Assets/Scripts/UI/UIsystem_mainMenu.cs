9bcabeafbf8be83adead7ef7f2ed70decb7df00a
priceEXE <152944571+priceEXE@users.noreply.github.com>
2024-07-28 16:58:34 +0800

    制作了一部分UI

--- Assets/Scripts/UI/UIsystem_mainMenu.cs
+++ Assets/Scripts/UI/UIsystem_mainMenu.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIsystem_mainMenu : MonoBehaviour
 {
+    public Image Volumn_image;
+    public Sprite[] Volumn;
+    public int volume_value=5;
+    public GameObject BGM;
     private bool isSetiingGame = false;
     public Animator titlePanel;
     public Animator SettingPenel;
+
+    public Animator OptionPanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +23,7 @@ public class UIsystem_mainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape) && isSetiingGame)
-        {
-            Debug.Log("返回标题面");
-            isSetiingGame = false;
-            titlePanel.SetBool("CouldActive",isSetiingGame);
-            SettingPenel.SetBool("CouldActive",isSetiingGame);
-        }
+        updateVolumn();
     }
 
     public void StartGame()
@@ -31,7 +32,49 @@ public class UIsystem_mainMenu : MonoBehaviour
         Debug.Log("strat game");
         titlePanel.SetBool("CouldActive",isSetiingGame);
         SettingPenel.SetBool("CouldActive",isSetiingGame);
+    }
 
-        
+    public void Return()
+    {
+        Debug.Log("返回标题面");
+        isSetiingGame = false;
+        titlePanel.SetBool("CouldActive",isSetiingGame);
+        SettingPenel.SetBool("CouldActive",isSetiingGame);
+    }
+
+    public void OptionPanelActive()
+    {
+        SettingPenel.SetBool("Option",true);
+        OptionPanel.SetBool("CouldActive",true);
+    }
+
+    public void ReturnOptionPanel()
+    {
+        SettingPenel.SetBool("Option",false);
+        OptionPanel.SetBool("CouldActive",false);
+    }
+
+    public void AddVolumn()
+    {
+        if(volume_value<10)
+        {
+            volume_value += 1 ;
+            Volumn_image.sprite = Volumn[10-volume_value];
+        }
+
+    }
+
+    public void ReduceVolumn()
+    {
+        if(volume_value>0)
+        {
+            volume_value -= 1;
+            Volumn_image.sprite = Volumn[10-volume_value];
+        }
+    }
+
+    public void updateVolumn()
+    {
+        BGM.GetComponent<AudioSource>().volume = volume_value*0.1f;
     }
 }
